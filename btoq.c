f109a41a2b513d6cc6f514afc949e0a0a9850dca
Nelson Morales <blancomorales@proton.me>
2023-09-13 09:46:22 -0400

    Create qtob.c, btoq.c

---
+++ btoq.c
@@ -0,0 +1,39 @@
+#include "qnum.h"
+
+static int ctoi(register char c)
+{
+	if ('0' <= c && c <= '9')
+		return c - '0';
+	if ('a' <= c && c <= 'z')
+		return c - 'a' + 10;
+	return ((unsigned) ~0) >> 1;
+}
+
+qnum btoq(const char *s, int b)
+{
+	int sign, dot;
+	qnum n = {0, 1};
+
+	while (*s == ' ' || *s == '\t')
+		++s;
+	sign = (*s == '-') ? -1 : 1;
+	if (*s == '-' || *s == '+')
+		++s;
+
+	for (dot = 0; *s != '\0'; ++s) {
+		if (*s == '.' && !dot) {
+			dot = 1;
+			continue;
+		}
+		if (ctoi(*s) >= b)
+			break;
+		n.num = n.num * b + ctoi(*s);
+		if (dot)
+			n.den = n.den * b;
+	}
+
+	qnum_reduce(&n);
+	n.num *= sign;
+
+	return n;
+}
---
+++ qtob.c
@@ -0,0 +1,47 @@
+#include "qnum.h"
+
+static char buf[(sizeof(long) + 1) * 8];
+static char *endc = buf + sizeof(buf) - 1;
+
+static char itoc(register int i)
+{
+	if (0 <= i && i < 10)
+		return i + '0';
+	if (10 <= i && i < 36)
+		return i + 'a' - 10;
+	return '\0';
+}
+
+const char *qtob(qnum n, int b)
+{
+	long ip;
+	int neg = n.num < 0;
+	char *s = buf, *t = endc;
+
+	if (neg)
+		n.num = -n.num;
+
+	ip = n.num / n.den;
+	do {
+		*--t = itoc(ip % b);
+	} while ((ip /= b) > 0);
+	if (neg)
+		*--t = '-';
+
+	while (t < endc)
+		*s++ = *t++;
+
+	if (n.den > 1) {
+		*s++ = '.';
+		n.num %= n.den;
+		do {
+			n.num = n.num * b;
+			qnum_reduce(&n);
+			*s++ = itoc((n.num / n.den) % b);
+		} while (n.den > 1 && s < endc);
+	}
+
+	*s = '\0';
+
+	return buf;
+}
